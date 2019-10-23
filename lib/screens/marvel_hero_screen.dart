c8acb048c7bec979c7f8e3b4421e44009d888b14
n1kk0 <nicolas.moussikian@gmail.com>
2019-10-23 22:45:14 +0200

    full pagination on hero view

--- lib/screens/marvel_hero_screen.dart
+++ lib/screens/marvel_hero_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 import 'package:marvel_client/providers/marvel_characters.dart';
+import 'package:marvel_client/models/marvel_character.dart';
 
 class MarvelHeroScreen extends StatefulWidget {
   final String _apiBaseUrl;
@@ -24,6 +25,7 @@ class _MarvelHeroScreenState extends State<MarvelHeroScreen> {
 
     return Scaffold(
       body: Swiper(
+        loop: false,
         itemBuilder: (BuildContext context,int index){
           return Center(
             child: Column(
@@ -42,16 +44,12 @@ class _MarvelHeroScreenState extends State<MarvelHeroScreen> {
             ),
           );
         },
-        itemCount: characters.lastPageLoaded * 15 < characters.marvelCharactersQuantity ?
-          characters.lastPageLoaded * 15:
-          characters.marvelCharactersQuantity
-        ,
+        itemCount: characters.marvelCharactersQuantity,
         onTap: (_) => Navigator.of(context).pop(),
         index: widget._index,
         controller: _controller,
-        control: SwiperControl(),
-//        pagination: SwiperPagination(builder: SwiperPagination.fraction),
-        pagination: YourOwnPaginatipon(),
+        control: MyControl(Provider.of<MarvelCharacters>(context, listen: false), widget._apiBaseUrl),
+        pagination: MyPaginatipon(),
       ),
     );
   }
@@ -73,25 +71,106 @@ class _MarvelHeroScreenState extends State<MarvelHeroScreen> {
   }
 }
 
-class YourOwnPaginatipon extends SwiperPlugin {
-  final EdgeInsetsGeometry margin = const EdgeInsets.all(10.0);
-  final SwiperPlugin builder = const FractionPaginationBuilder(color: Colors.black45);
-
+class MyPaginatipon extends SwiperPlugin {
   Widget build(BuildContext context, SwiperPluginConfig config) {
-    final Alignment alignment = config.scrollDirection == Axis.horizontal ? Alignment.bottomCenter : Alignment.centerRight;
-
-    Widget child = Container(
-      margin: margin,
-      child: this.builder.build(context, config),
+    return Align(
+      alignment: config.scrollDirection == Axis.horizontal ? Alignment.bottomCenter : Alignment.centerRight,
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        child: FractionPaginationBuilder(color: Colors.black45).build(context, config),
+      ),
     );
+  }
+}
 
-    if (!config.outer) {
-      child = new Align(
-        alignment: alignment,
-        child: child,
+class MyControl extends SwiperPlugin {
+  final MarvelCharacters characters;
+  final String _apiBaseUrl;
+
+  MyControl(this.characters, this._apiBaseUrl);
+
+  Widget buildButton(SwiperPluginConfig config, Color color, IconData iconData, int quarterTurns, bool previous) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () async {
+        if (previous) {
+          config.controller.previous(animation: true);
+        } else {
+          final int lastItemLoaded = characters.lastPageLoaded * 15 - 2;
+          final int currentIndex = config.activeIndex;
+
+          if (currentIndex == lastItemLoaded) {
+            await characters.loadPage(() {}, () {}, _imagePreloader);
+          }
+
+          config.controller.next(animation: true);
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: RotatedBox(
+          quarterTurns: quarterTurns,
+          child: Icon(
+            iconData,
+            semanticLabel: previous ? "Previous" : "Next",
+            size: 30.0,
+            color: color,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, SwiperPluginConfig config) {
+    ThemeData themeData = Theme.of(context);
+
+    Color color = themeData.primaryColor;
+    Color disableColor = themeData.disabledColor;
+    Color prevColor;
+    Color nextColor;
+
+    if (config.loop) {
+      prevColor = nextColor = color;
+    } else {
+      bool next = config.activeIndex < config.itemCount - 1;
+      bool prev = config.activeIndex > 0;
+      prevColor = prev ? color : disableColor;
+      nextColor = next ? color : disableColor;
+    }
+
+    Widget child;
+
+    if (config.scrollDirection == Axis.horizontal) {
+      child = Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          buildButton(config, prevColor, Icons.arrow_back_ios, 0, true),
+          buildButton(config, nextColor, Icons.arrow_forward_ios, 0, false)
+        ],
+      );
+    } else {
+      child = Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          buildButton(config, prevColor, Icons.arrow_back_ios, -3, true),
+          buildButton(config, nextColor, Icons.arrow_forward_ios, -3, false)
+        ],
       );
     }
 
-    return child;
+    return Container(
+      height: double.infinity,
+      child: child,
+      width: double.infinity,
+    );
+  }
+
+  Null _imagePreloader(MarvelCharacter marvelCharacter) {
+    final Image image = marvelCharacter.getImage("$_apiBaseUrl/images?uri=");
+
+    image.image.resolve(ImageConfiguration()).addListener(ImageStreamListener((_, __) {
+        marvelCharacter.loaded = true;
+    }));
   }
 }
